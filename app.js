fd84fa16dde27728071dcc224b19327d32981f7e
Ayan Aliyeva <aliyevayan1929@gmail.com>
2025-10-26 11:30:28 +0400

    commit

--- app.js
+++ app.js
@@ -1,19 +1,75 @@
+const products = [
+    ["Lenovo IdeaPad 5 Laptop", 555.99, "Ultrabook with AMD Ryzen 5 and 15.6\" display", 2021],
+    ["Samsung Galaxy S24 Smartphone", 999.99, "Flagship phone with AMOLED display and triple camera", 2022],
+  ];
+ 
+  // 1. massive yeni mal elave etmek - Add
+  // 2. massivden ada gore mal silmek  - Delete
+  // 3. qiymete gore sort etmek - sort price
+  // 4. ile gore sort etmek imkani olmalidir - sort year
+  // 5. malin qiymetini deyishmek imkani olmalidir - change
+  // 5. mallari gostermek imkani olmalidir - Show
+ 
+  while   (true){
+      let input=prompt("1.Add new product\n2.Delete product\n3.Show products\n4.Sort by price\n5.Sort by year\n6.Change the product price")
+      // add product
+      if(input=='1'){
+          let name=prompt("Enter the new product name")
+          let price=prompt("Enter the new product price")
+          let description=prompt("Enter new product description")
+          let year=prompt("Enter the new product year")
+          let product=[name,parseFloat(price),description,parseInt(year)];
+          products.push(product)
+          alert("Product succesfully added!")
+      }
 
-let metn=`New York, often called New York City[b] or NYC, is the most populous city in the United States, located at the southern tip of New York State on one of the world's largest natural harbors. The city comprises five boroughs, each coextensive with a respective county. New York is a global center of finance[11] and commerce, culture, technology,[12] entertainment and media, academics and scientific output,[13] the arts and fashion, and, as home to the headquarters of the United Nations, international diplomacy.[14][15][16][17][18]
+      // delete product
+      else if(input=='2'){
+         let productt = prompt("Enter product name");
+     
+         let findIndex = products.findIndex(item => item[0] === productt);
+     
+         if (findIndex === -1) {
+           alert("Product not found");
+         } else {
+           products.splice(findIndex, 1);
+           alert("Product deleted successfully");
+         }
+       }
+       // show product
+      else if(input=='3'){
+        alert(products.map(item =>
+        `Product name: ${item[0]}\nPrice: ${item[1]}\nDescription: ${item[2]}\nYear: ${item[3]}\n------------------------------------------------------------`).join("\n"));  
+    }
+      // sort product by price
+      else if(input=='4'){
+       products.sort((a, b) => a[1] - b[1]);
+          alert(products.map(item =>
+      `Product name: ${item[0]}\nPrice: ${item[1]}\nDescription: ${item[2]}\nYear: ${item[3]}\n------------------------------------------------------------`
+      ).join("\n")
+         );    
+       }
 
-With an estimated population in 2023 of 8,258,035[5] distributed over 300.46 square miles (778.2 km2),[4] the city is the most densely populated major city in the United States. New York City has more than double the population of Los Angeles, the nation's second-most populous city.[19] New York is the geographical and demographic center of both the Northeast megalopolis and the New York metropolitan area, the largest metropolitan area in the U.S. by both population and urban area. With more than 20.1 million people in its metropolitan statistical area[20] and 23.5 million in its combined statistical area as of 2020, New York City is one of the world's most populous megacities.[21] The city and its metropolitan area are the premier gateway for legal immigration to the United States. As many as 800 languages are spoken in New York City,[22] making it the most linguistically diverse city in the world. In 2021, the city was home to nearly 3.1 million residents born outside the U.S.,[19] the largest foreign-born population of any city in the world.[23]
+       // sort by year
+       else if(input=='5'){
+        products.sort((a, b) => a[3] - b[3]);
+        alert(products.map(item =>
+        `Product name: ${item[0]}\nPrice: ${item[1]}\nDescription: ${item[2]}\nYear: ${item[3]}\n------------------------------------------------------------`).join("\n"));  
+       }
 
-New York City traces its origins to Fort Amsterdam and a trading post founded on Manhattan Island by Dutch colonists around 1624. The settlement was named New Amsterdam in 1626 and was chartered as a city in 1653. The city came under English control in 1664 and was temporarily renamed New York after King Charles II granted the lands to his brother, the Duke of York,[24] before being permanently renamed New York in November 1674. New York City was the U.S. capital from 1785 until 1790.[25] The modern city was formed by the 1898 consolidation of its five boroughs: Manhattan, Brooklyn, Queens, The Bronx, and Staten Island.
+       //change
+      else if(input=='6'){
+        let productNamee=prompt("Enter product name ")
+        let findIndex = products.findIndex(item => item[0] === productNamee);
+        if(findIndex==-1){
+            alert("Product not found");
+        }
+        else{
+            let newPrice=prompt("enter new price")
+            products[findIndex][1]= parseFloat(newPrice);
+            alert("product updated successfully")
+        }
+    }
+  
+  }
 
-Anchored by Wall Street in the Financial District, Manhattan, New York City has been called both the world's premier financial and fintech center[26][27] and the most economically powerful city in the world.[28] As of 2022, the New York metropolitan area is the largest metropolitan economy in the world, with a gross metropolitan product of over US$2.16 trillion.[9] If the New York metropolitan area were its own country, it would have the tenth-largest economy in the world. The city is home to the world's two largest stock exchanges by market capitalization of their listed companies: the New York Stock Exchange and Nasdaq. New York City is an established safe haven for global investors.[29] As of 2023, New York City is the most expensive city in the world for expatriates[30] and has by a wide margin the highest U.S. city residential rents;[31] and Fifth Avenue is the most expensive shopping street in the world.[32] New York City is home by a significant margin to the highest number of billionaires,[33] individuals of ultra-high net worth (greater than US$30 million),[34] and millionaires of any city in the world.`;
-
-
-let count=0;
-for (let index = 0; index < metn.length; index++) {
-    let soz=metn.substring(index,index+8)
-
-    if(soz==="New York"){
-        count++
-    } 
-}
-console.log(count)
